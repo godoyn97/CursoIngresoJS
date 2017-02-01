fcfb07bbea1c30b11f5a424ce53a126c9c5ffe32
godoyn97 <godoyn97@gmail.com>
2017-02-01 16:58:53 +0200

    Add files via upload

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,25 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
+function Mostar()
+{
+	//esto es un comentario de una linea
+	/*bl
+	w3school*/
+		/*variables, una porcion de la memoria ram, un cuadrado, almacenan datos
+	definir
+	 variables almacenan datos
+	 variable var (nombre de la variable);
+	 siempre un renglon bajo el otro*/
+	var nombre; //defino variable. abajo se da un valor.
+	//nombre = "Nahuel Godoy"; //Cargo variable. se cita el nombre = valor.
+	
+	nombre = prompt("ingrese su nombre", "Natalia Natalia");
+	/*prompt devuelve un valor. alert donde se le cante. el prompt despues de la variable*/
+	alert("nombre: " + nombre); /* double "" da un valor literal, no habla de la variable
+	el espacio se da dentro de las comillas
+	contatecnar: tomar valores literales con valores variables*/
+	/*var person = prompt("Ingresa tu nombre", "Natalia Natalia")
+	octavio.com.ar;*/
+
+}
+
