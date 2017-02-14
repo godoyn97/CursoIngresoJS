512bf1f54cc2e922a4935efc14e4408c87bd45e2
godoyn97 <godoyn97@gmail.com>
2017-02-14 12:07:36 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -3,7 +3,26 @@ function Mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
+switch(mesDelAño)
+{
+	case "Enero":
+		alert("que comiences bien el año!!!");
+		break;
+	
+	case "Marzo":
+		alert("A clases!!!");
+		break;
 
+	case "Julio":
+		alert("Se vienen las vacaciones!!!");
+		break;
+
+	case "Diciembre":
+		alert("Felices fiestas");
+		break;	
+
+
+}
 
 
 }//FIN DE LA FUNCIÓN
