6c3b761281a745e8a67c163b69ee311a1a2dd59d
godoyn97 <godoyn97@gmail.com>
2017-02-14 12:06:22 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,33 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+		var nRandom;
 
+		nRandom = Math.floor((Math.random()*10)+1);
+
+		if(nRandom>8)
+		{
+			alert("excelente " );
+		}
+		else
+		{
+			if(nRandom < 4)
+			{
+				alert("vamos, la porxima se puede  ");
+			}
+			else
+			{
+				alert("aprobó ");
+			}
+		}
+
+		/*if(nRandom>3 && nRandom< 9)
+		{
+			alert("aprobó " + nRandom);
+		}
+		else*/
+		/*else
+		{
+			alert("vamos, la proxima se puede " + nRandom);
+		}*/
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,13 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+	var numero;
 	
+	numero = Math.floor((Math.random() * 10)+1);
 
+	alert(numero);
 }//FIN DE LA FUNCIÓN
+/*para numeros enteros math.floor()
+math.floor((math.random()*10)+ 1);    // numeros del 1(al 0 le suma 1) al 10 
+Math.floor(Math.random() * 11);      // devuelve un numero del 0 al 10
+Math.floor(Math.random() * 100);     // devuelve un numero del 0 al 99 */
