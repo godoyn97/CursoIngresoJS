20080c5727ad3e1c8083d64bdae437279d8afce7
godoyn97 <godoyn97@gmail.com>
2017-02-18 12:59:47 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -3,8 +3,23 @@ function Mostrar()
 
 	var contador=0;
 	var acumulador=0;
+	var numero;
+
+	//numero= prompt("ingrese");
+
+	while(contador< 5)
+	{
+		numero=prompt("ingrese");
+		
+		numero=parseInt(numero);
+
+		acumulador= acumulador + numero;
+		
+		contador++;
 
 
+
+	}
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/5;
--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -4,7 +4,23 @@ function Mostrar()
 	var contador=0;
 	var acumulador=0;
 	var respuesta='si';
+	var numero;
+	
 
+
+	while(respuesta != 'si')
+	{
+		respuesta= prompt("informar suma?");
+
+		numero=prompt("ingrese numero:");
+
+		numero = parseInt(numero);
+
+
+	
+
+
+	}
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/contador;
